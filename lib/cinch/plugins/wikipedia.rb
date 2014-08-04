b106d0b067600e5c9ad0dad4e6819a1857fc33e6
Brian Haberer <bhaberer@gmail.com>
2014-08-04 02:12:22 -0700

    v1.0.4 - Spec refactor

--- lib/cinch/plugins/wikipedia.rb
+++ lib/cinch/plugins/wikipedia.rb
@@ -3,64 +3,65 @@ require 'cinch'
 require 'cinch/toolbox'
 require 'cinch/cooldown'
 
-module Cinch::Plugins
-  # Plugin to allow users to search wikipedia.
-  class Wikipedia
-    include Cinch::Plugin
+module Cinch
+  module Plugins
+    # Plugin to allow users to search wikipedia.
+    class Wikipedia
+      include Cinch::Plugin
 
-    enforce_cooldown
+      enforce_cooldown
 
-    self.help = 'Use .wiki <term> to see the Wikipedia info for that term.'
+      self.help = 'Use .wiki <term> to see the Wikipedia info for that term.'
 
-    match /wiki (.*)/
-    match /wikipedia (.*)/
+      match(/wiki(?:pedia)? (.*)/)
 
-    def initialize(*args)
-      super
-      @max_length = config[:max_length] || 300
-    end
+      def initialize(*args)
+        super
+        @max_length = config[:max_length] || 300
+      end
 
-    def execute(m, term)
-      m.reply wiki(term)
-    end
+      def execute(m, term)
+        m.reply wiki(term)
+      end
 
-    private
+      private
 
-    def wiki(term)
-      # URI Encode
-      term = URI.escape(term, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
-      url = "http://en.wikipedia.org/w/index.php?search=#{term}"
+      def wiki(term)
+        # URI Encode
+        term = URI.escape(term, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
+        url = "http://en.wikipedia.org/w/index.php?search=#{term}"
 
-      # Truncate text and url if they are too long
-      text = Cinch::Toolbox.truncate(get_def(term, url), @max_length)
-      url  = Cinch::Toolbox.shorten(url)
+        # Truncate text and url if they are too long
+        text = Cinch::Toolbox.truncate(get_def(term, url), @max_length)
+        url  = Cinch::Toolbox.shorten(url)
 
-      "Wikipedia ∴ #{text} [#{url}]"
-    end
+        "Wikipedia ∴ #{text} [#{url}]"
+      end
 
-    def get_def(term, url)
-      cats = Cinch::Toolbox.get_html_element(url, '#mw-normal-catlinks')
-      if cats && cats.include?('Disambiguation')
-        wiki_text = "'#{term} is too vague and lead to a disambiguation page."
-      else
-        wiki_text = Cinch::Toolbox.get_html_element(url, '#mw-content-text p')
-        if wiki_text.nil? || wiki_text.include?('Help:Searching')
-          return not_found(wiki_text, url)
+      def get_def(term, url)
+        cats = Cinch::Toolbox.get_html_element(url, '#mw-normal-catlinks')
+        if cats && cats.include?('Disambiguation')
+          wiki_text = "'#{term} is too vague and lead to a disambiguation page."
+        else
+          wiki_text = Cinch::Toolbox.get_html_element(url, '#mw-content-text p')
+          if wiki_text.nil? || wiki_text.include?('Help:Searching')
+            return not_found(url)
+          end
         end
+        wiki_text
       end
-      wiki_text
-    end
 
-    def not_found(wiki_text, url)
-      msg = "I couldn't find anything for that search, "
-      alt_term_text = Cinch::Toolbox.get_html_element(url, '.searchdidyoumean')
-      if alt_term_text
-        alt_term = alt_term_text[/\ADid you mean: (\w+)\z/, 1]
-        msg << "did you mean '#{alt_term}'?"
-      else
-        msg << 'sorry!'
+      def not_found(url)
+        msg = "I couldn't find anything for that search, "
+        alt_term = Cinch::Toolbox.get_html_element(url, '.searchdidyoumean')
+        if alt_term
+          alt_term = alt_term[/\ADid you mean: (\w+)\z/, 1]
+          msg << "did you mean '#{alt_term}'?"
+        else
+          msg << 'sorry!'
+        end
+        msg
       end
-      msg
     end
   end
 end
--- spec/cinch-wikipedia_spec.rb
+++ spec/cinch-wikipedia_spec.rb
@@ -9,34 +9,41 @@ describe Cinch::Plugins::Wikipedia do
 
   # normal queries
   it 'should return a definition of a term' do
-    get_replies(make_message(@bot, '!wiki computer')).first.text.
-      should include("A computer is a general purpose device")
+    expect(get_replies(make_message(@bot, '!wiki computer')).first.text)
+      .to include("A computer is a general purpose device")
+  end
+
+  it 'should return a definition of a term for each version of cmd' do
+    msg1 = get_replies(make_message(@bot, '!wiki computer')).first.text
+    msg2 = get_replies(make_message(@bot, '!wikipedia computer')).first.text
+    expect(msg1).to eq(msg2)
   end
 
   it 'should not return multiple lined definitions' do
-    get_replies(make_message(@bot, '!wiki Teenager')).first.text.
-      should_not include("\n")
+    expect(get_replies(make_message(@bot, '!wiki Teenager')).first.text)
+      .to_not include("\n")
   end
 
   it 'should not return definitions that are longer than 250 chars' do
-    get_replies(make_message(@bot, '!wiki Teenager')).first.text.length.
-      should == 334
+    expect(get_replies(make_message(@bot, '!wiki Teenager')).first.text.length)
+      .to eq 334
   end
 
   # Not found
   it 'should return an error message when a term is not found' do
-    get_replies(make_message(@bot, '!wiki dasdafasfasfasfasafsdfsdfsadf')).first.text.
-      should include("I couldn't find anything for that search, sorry!")
+    msg = make_message(@bot, '!wiki dasdafasfasfasfasafsdfsdfsadf')
+    expect(get_replies(msg).first.text)
+      .to include("I couldn't find anything for that search, sorry!")
   end
 
   it 'should provide suggestions if one is listed on the page' do
-    get_replies(make_message(@bot, '!wiki smegama')).first.text.
-      should include("I couldn't find anything for that search, did you mean 'smegma'?")
+    expect(get_replies(make_message(@bot, '!wiki smegama')).first.text)
+      .to include("I couldn't find anything for that search, did you mean 'smegma'?")
   end
 
   # disambiguation
   it 'should return helful information when a disambuation page' do
-    get_replies(make_message(@bot, '!wiki hacker')).first.text.
-      should include('is too vague and lead to a disambiguation page')
+    expect(get_replies(make_message(@bot, '!wiki hacker')).first.text)
+      .to include('is too vague and lead to a disambiguation page')
   end
 end
